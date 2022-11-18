93e7d81e65f6152066abb8d1271ffb769c2c5f5f
Mikhail Morozov <plzombie@yandex.ru>
2022-11-18 19:23:35 +0300

    Refactoring

--- fastimage.c
+++ fastimage.c
@@ -44,10 +44,176 @@ typedef struct {
 	unsigned char NPlanes;
 } pcx_header_min_t;
 
+
+static void fastimageReadBmp(const fastimage_reader_t *reader, unsigned char *sign, fastimage_image_t *image)
+{
+	BITMAPINFOHEADER bmp_infoheader;
+	
+	(void)sign; // Unused
+		
+	if(!reader->seek(reader->context, sizeof(BITMAPFILEHEADER))) {
+		image->format = fastimage_error;
+
+		return;
+	}
+
+	if(reader->read(reader->context, sizeof(BITMAPINFOHEADER), &bmp_infoheader) != sizeof(BITMAPINFOHEADER)) {
+		image->format = fastimage_error;
+
+		return;
+	}
+
+	image->width = bmp_infoheader.biWidth;
+	image->height = bmp_infoheader.biHeight;
+	switch(bmp_infoheader.biBitCount) {
+		case 16:
+		case 24:
+		case 32:
+			image->bpp = bmp_infoheader.biBitCount / 8;
+			break;
+		case 1:
+		case 4:
+		case 8:
+			image->bpp = 4;
+			image->palette = bmp_infoheader.biBitCount;
+			break;
+		default:
+			image->format = fastimage_error;
+	}
+}
+
+static void fastimageReadPcx(const fastimage_reader_t *reader, unsigned char *sign, fastimage_image_t *image)
+{
+	pcx_header_min_t pcx_header_min;
+
+	(void)sign; // Unused
+
+	if(reader->read(reader->context, sizeof(pcx_header_min_t), &pcx_header_min) != sizeof(pcx_header_min_t)) {
+		image->format = fastimage_error;
+
+		return;
+	}
+
+	image->width = (size_t)(pcx_header_min.Xmax - pcx_header_min.Xmin) + 1;
+	image->height = (size_t)(pcx_header_min.Ymax - pcx_header_min.Ymin) + 1;
+	image->bpp = 3;
+
+	if(pcx_header_min.NPlanes == 1)
+		image->palette = 8;
+	else if(pcx_header_min.NPlanes != 3)
+		image->format = fastimage_error;
+}
+
+static void fastimageReadGif(const fastimage_reader_t *reader, unsigned char *sign, fastimage_image_t *image)
+{
+	unsigned short gif_header_min[3];
+	
+	(void)sign; // Unused
+	
+	// GIF87a or GIF89a
+	if(reader->read(reader->context, 6, &gif_header_min) != 6) {
+		image->format = fastimage_error;
+		
+		return;
+	}
+		
+	if( (gif_header_min[0] != '7'+(unsigned short)('a')*256)
+		&& (gif_header_min[0] != '9'+(unsigned short)('a')*256)) {
+		image->format = fastimage_error;
+			
+		return;
+	}
+		
+	image->width = gif_header_min[1];
+	image->height = gif_header_min[2];
+	image->bpp = 3;
+	image->palette = 8;
+}
+
+static void fastimageReadJpeg(const fastimage_reader_t *reader, unsigned char *sign, fastimage_image_t *image)
+{
+	int64_t jpg_curr_offt = 4;
+	unsigned short jpg_frame;
+	int64_t jpg_segment_size;
+		
+	jpg_frame = sign[2]+(unsigned short)(sign[3])*256;
+		
+	// First of all, skipping segments we don't needed
+	// TODO: there are segments without length (with empty or 2-byte data). Need to check them
+	while(1) {
+		unsigned char jpg_bytes[2];
+			
+		//printf("jpeg segment %hx\n", jpg_frame);
+			
+		// Read segment size
+		if(reader->read(reader->context, 2, jpg_bytes) != 2) {
+			image->format = fastimage_error;
+				
+			return;
+		}
+			
+		jpg_curr_offt += 2;
+			
+		jpg_segment_size = (int64_t)(jpg_bytes[0])*256+jpg_bytes[1];
+			
+		if(jpg_segment_size < 2) {
+			image->format = fastimage_error;
+				
+			return;
+		}
+			
+		jpg_segment_size -= 2;
+			
+		if(jpg_frame == 0xC0FF || jpg_frame == 0xC1FF || jpg_frame == 0xC2FF) {
+			break;
+		}
+			
+		// Skip segment
+		jpg_curr_offt += jpg_segment_size;
+			
+		if(!reader->seek(reader->context, jpg_curr_offt)) {
+			image->format = fastimage_error;
+				
+			return;
+		}
+			
+		// Read next segment signature
+		if(reader->read(reader->context, 2, &jpg_frame) != 2) {
+			image->format = fastimage_error;
+				
+			return;
+		}
+		
+		jpg_curr_offt += 2;
+	}
+		
+	if(jpg_frame == 0xC0FF || jpg_frame == 0xC1FF || jpg_frame == 0xC2FF) {
+		// Found segment with header
+		unsigned char jpg_bytes[6];
+		
+		if(jpg_segment_size < 6) {
+			image->format = fastimage_error;
+					
+			return;
+		}
+				
+		if(reader->read(reader->context, 6, jpg_bytes) != 6) {
+			image->format = fastimage_error;
+				
+			return;
+		}
+			
+		image->width = (size_t)(jpg_bytes[1])*256+jpg_bytes[2];
+		image->height = (size_t)(jpg_bytes[3])*256+jpg_bytes[4];
+		image->bpp = jpg_bytes[5];
+	} else
+		image->format = fastimage_error;
+}
+
 fastimage_image_t fastimageOpen(const fastimage_reader_t *reader)
 {
 	fastimage_image_t image;
-	char sign[4];
+	unsigned char sign[4];
 	
 	memset(&image, 0, sizeof(fastimage_image_t));
 	
@@ -79,88 +245,22 @@ fastimage_image_t fastimageOpen(const fastimage_reader_t *reader)
 	// Here should be code
 	
 	// Read BMP meta
-	if(image.format == fastimage_bmp) {
-		BITMAPINFOHEADER bmp_infoheader;
-		
-		if(!reader->seek(reader->context, sizeof(BITMAPFILEHEADER))) {
-			image.format = fastimage_error;
-			
-			goto FINAL;
-		}
-		
-		if(reader->read(reader->context, sizeof(BITMAPINFOHEADER), &bmp_infoheader) != sizeof(BITMAPINFOHEADER)) {
-			image.format = fastimage_error;
-			
-			goto FINAL;
-		}
-		
-		image.width = bmp_infoheader.biWidth;
-		image.height = bmp_infoheader.biHeight;
-		switch(bmp_infoheader.biBitCount) {
-			case 16:
-			case 24:
-			case 32:
-				image.bpp = bmp_infoheader.biBitCount / 8;
-				break;
-			case 1:
-			case 4:
-			case 8:
-				image.bpp = 4;
-				image.palette = bmp_infoheader.biBitCount;
-				break;
-			default:
-				image.format = fastimage_error;
-		}
-	}
+	if(image.format == fastimage_bmp)
+		fastimageReadBmp(reader, sign, &image);
 	
 	// Read TGA meta
 	// Here should be code
 	
 	// Read PCX meta
-	if(image.format == fastimage_pcx) {
-		pcx_header_min_t pcx_header_min;
-		
-		if(reader->read(reader->context, sizeof(pcx_header_min_t), &pcx_header_min) != sizeof(pcx_header_min_t)) {
-			image.format = fastimage_error;
-			
-			goto FINAL;
-		}
-		
-		image.width = (size_t)(pcx_header_min.Xmax - pcx_header_min.Xmin) + 1;
-		image.height = (size_t)(pcx_header_min.Ymax - pcx_header_min.Ymin) + 1;
-		image.bpp = 3;
-			
-		if(pcx_header_min.NPlanes == 1)
-			image.palette = 8;
-		else if(pcx_header_min.NPlanes != 3)
-			image.format = fastimage_error;
-	}
+	if(image.format == fastimage_pcx)
+		fastimageReadPcx(reader, sign, &image);
 	
 	// Read PNG meta
 	// Here should be code
 	
 	// Read GIF meta
-	if(image.format == fastimage_gif) {
-		unsigned short gif_header_min[3];
-		// GIF87a or GIF89a
-		if(reader->read(reader->context, 6, &gif_header_min) != 6) {
-			image.format = fastimage_error;
-			
-			goto FINAL;
-		}
-		
-		if( (gif_header_min[0] != '7'+(unsigned short)('a')*256)
-			&& (gif_header_min[0] != '9'+(unsigned short)('a')*256)) {
-			image.format = fastimage_error;
-			
-			goto FINAL;
-		}
-		
-		image.width = gif_header_min[1];
-		image.height = gif_header_min[2];
-		image.bpp = 3;
-		image.palette = 8;
-	}
+	if(image.format == fastimage_gif)
+		fastimageReadGif(reader, sign, &image);
 	
 	// Read WEBP meta
 	// Here should be code
@@ -169,86 +269,8 @@ fastimage_image_t fastimageOpen(const fastimage_reader_t *reader)
 	// Here should be code
 	
 	// Read JPG meta
-	if(image.format == fastimage_jpg) {
-		int64_t jpg_curr_offt = 4;
-		unsigned short jpg_frame;
-		int64_t jpg_segment_size;
-		
-		jpg_frame = sign[2]+(unsigned short)(sign[3])*256;
-		
-		// First of all, skipping segments we don't needed
-		// TODO: there are segments without length (with empty or 2-byte data). Need to check them
-		while(1) {
-			unsigned char jpg_bytes[2];
-			
-			//printf("jpeg segment %hx\n", jpg_frame);
-			
-			// Read segment size
-			if(reader->read(reader->context, 2, jpg_bytes) != 2) {
-				image.format = fastimage_error;
-				
-				goto FINAL;
-			}
-			
-			jpg_curr_offt += 2;
-			
-			jpg_segment_size = (int64_t)(jpg_bytes[0])*256+jpg_bytes[1];
-			
-			if(jpg_segment_size < 2) {
-				image.format = fastimage_error;
-				
-				goto FINAL;
-			}
-			
-			jpg_segment_size -= 2;
-			
-			if(jpg_frame == 0xC0FF || jpg_frame == 0xC1FF || jpg_frame == 0xC2FF) {
-				break;
-			}
-			
-			// Skip segment
-			jpg_curr_offt += jpg_segment_size;
-			
-			if(!reader->seek(reader->context, jpg_curr_offt)) {
-				image.format = fastimage_error;
-				
-				goto FINAL;
-			}
-			
-			// Read next segment signature
-			if(reader->read(reader->context, 2, &jpg_frame) != 2) {
-				image.format = fastimage_error;
-				
-				goto FINAL;
-			}
-			
-			jpg_curr_offt += 2;
-		}
-		
-		if(jpg_frame == 0xC0FF || jpg_frame == 0xC1FF || jpg_frame == 0xC2FF) {
-			// Found segment with header
-			unsigned char jpg_bytes[6];
-			
-			if(jpg_segment_size < 6) {
-				image.format = fastimage_error;
-					
-				goto FINAL;
-			}
-				
-			if(reader->read(reader->context, 6, jpg_bytes) != 6) {
-				image.format = fastimage_error;
-					
-				goto FINAL;
-			}
-			
-			image.width = (size_t)(jpg_bytes[1])*256+jpg_bytes[2];
-			image.height = (size_t)(jpg_bytes[3])*256+jpg_bytes[4];
-			image.bpp = jpg_bytes[5];
-		} else
-			image.format = fastimage_error;
-	}
-	
-FINAL:
+	if(image.format == fastimage_jpg)
+		fastimageReadJpeg(reader, sign, &image);
 	
 	return image;
 }
